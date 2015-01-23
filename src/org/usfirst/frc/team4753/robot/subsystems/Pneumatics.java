966b81b4d6c614f1d597d9df9fb02b03cfae78fd
Nathan <nana8901@gmail.com>
2015-01-23 16:48:23 -0500

    Created Pneumatics, began process of shifting all references for pneumatic subsystems to Pneumatics subsystem

---
+++ src/org/usfirst/frc/team4753/robot/subsystems/Pneumatics.java
@@ -0,0 +1,74 @@
+package org.usfirst.frc.team4753.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Pneumatics extends Subsystem 
+{
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() 
+    {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    //=================================
+	Solenoid grabbing = new Solenoid(1);
+	Solenoid liftingOne = new Solenoid(2);
+	Solenoid liftingTwo = new Solenoid(3);
+	DoubleSolenoid pushBar = new DoubleSolenoid(1, 2);
+
+		// Put methods for controlling this subsystem
+		// here. Call these from Commands.
+
+
+		/**
+		 * Activates grabbing solenoid so that it grabs tote
+		 * 
+		 */
+		public void eject() {
+			pushBar.set(DoubleSolenoid.Value.kForward);
+		}
+
+		public void retract() {
+			pushBar.set(DoubleSolenoid.Value.kReverse);
+		}
+		
+		public void grab() {
+			grabbing.set(true);
+		}
+
+		/**
+		 * Retracts pistons so that tote rises false = raise true = lower
+		 */
+		public void raise() {
+			liftingOne.set(false);
+			liftingTwo.set(false);
+		}
+
+		/**
+		 * Releases pistons so that tote is released
+		 * 
+		 */
+		public void release() {
+			grabbing.set(false);
+		}
+
+		/**
+		 * activates pistons so that tote is lowered
+		 * 
+		 */
+		public void lower() {
+			liftingOne.set(true);
+			liftingTwo.set(true);
+		}
+	//>>>>>>> branch '2015' of https://github.com/BexleyRobotics/MecanumDrive.git
+    	}
+
+
